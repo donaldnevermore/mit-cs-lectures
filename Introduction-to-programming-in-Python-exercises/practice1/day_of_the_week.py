423aa58e8af1e644dba70bea613972645ae01c6a
wangdongcheng <1219782000@qq.com>
2018-02-06 23:04:11 +0800

    new file

--- Introduction-to-programming-in-Python-exercises/practice1/day_of_the_week.py
+++ Introduction-to-programming-in-Python-exercises/practice1/day_of_the_week.py
@@ -12,9 +12,12 @@ class Weekday(Enum):
     星期六 = 6
 
 
+anotherWeekDay = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+
 y = int(sys.argv[1])
 m = int(sys.argv[2])
 d = int(sys.argv[3])
+
 
 y0 = y-(14-m)//12
 x = y0+y0//4-y0//100+y0//400
@@ -22,3 +25,4 @@ m0 = m+12*((14-m)//12)-2
 d0 = (d+x+(31*m0)//12) % 7
 
 print('这一天是{}'.format(Weekday(d0).name))
+print(f'这一天是{anotherWeekDay[d0]}')
---
+++ Introduction-to-programming-in-Python-exercises/practice1/uniform_random_number.py
@@ -0,0 +1,12 @@
+import random
+import math
+from functools import reduce
+
+n = 5
+numbers = [random.random() for _ in range(n)]
+average = reduce(lambda x, y: x+y, numbers)/n
+small = min(numbers)
+big = max(numbers)
+
+print(numbers)
+print(average, small, big)
