6c3874bb0f86cbb7bb3f375b0b3010044ba1fd8c
wangdongcheng <1219782000@qq.com>
2018-02-07 23:36:04 +0800

    add file

---
+++ Introduction-to-programming-in-Python-exercises/practice1/mercator_projection.py
@@ -0,0 +1,13 @@
+import sys
+import math
+
+lam = float(sys.argv[1])
+lat = float(sys.argv[2])
+lng = float(sys.argv[3])
+
+x = lng-lam
+minus = (1+math.sin(lat))/(1-math.sin(lat))
+y = 1/2*math.log(minus)
+
+result = (x, y)
+print(result)
